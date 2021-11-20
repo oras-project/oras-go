9e36b1c8c34caa85e200895489b42a0e061bba73
Shiwei Zhang <shizh@microsoft.com>
2021-11-20 13:06:16 +0800

    copy test
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- copy_test.go
+++ copy_test.go
@@ -20,6 +20,7 @@ import (
 	_ "crypto/sha256"
 	"encoding/json"
 	"io"
+	"reflect"
 	"sync/atomic"
 	"testing"
 
@@ -27,6 +28,7 @@ import (
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"oras.land/oras-go"
 	"oras.land/oras-go/content"
+	"oras.land/oras-go/content/memory"
 	"oras.land/oras-go/internal/cas"
 )
 
@@ -51,6 +53,83 @@ func (t *storageTracker) Push(ctx context.Context, expected ocispec.Descriptor, 
 func (t *storageTracker) Exists(ctx context.Context, target ocispec.Descriptor) (bool, error) {
 	atomic.AddInt64(&t.exists, 1)
 	return t.Storage.Exists(ctx, target)
+}
+
+func TestCopy_FullCopy(t *testing.T) {
+	src := memory.New()
+	dst := memory.New()
+
+	// generate test content
+	var blobs [][]byte
+	var descs []ocispec.Descriptor
+	appendBlob := func(mediaType string, blob []byte) {
+		blobs = append(blobs, blob)
+		descs = append(descs, ocispec.Descriptor{
+			MediaType: mediaType,
+			Digest:    digest.FromBytes(blob),
+			Size:      int64(len(blob)),
+		})
+	}
+	generateManifest := func(config ocispec.Descriptor, layers ...ocispec.Descriptor) {
+		manifest := ocispec.Manifest{
+			Config: config,
+			Layers: layers,
+		}
+		manifestJSON, err := json.Marshal(manifest)
+		if err != nil {
+			t.Fatal(err)
+		}
+		appendBlob(ocispec.MediaTypeImageManifest, manifestJSON)
+	}
+
+	appendBlob(ocispec.MediaTypeImageConfig, []byte("config")) // Blob 0
+	appendBlob(ocispec.MediaTypeImageLayer, []byte("foo"))     // Blob 1
+	appendBlob(ocispec.MediaTypeImageLayer, []byte("bar"))     // Blob 2
+	generateManifest(descs[0], descs[1:3]...)                  // Blob 3
+
+	ctx := context.Background()
+	for i := range blobs {
+		err := src.Push(ctx, descs[i], bytes.NewReader(blobs[i]))
+		if err != nil {
+			t.Fatalf("failed to push test content to src: %d: %v", i, err)
+		}
+	}
+
+	root := descs[3]
+	ref := "foobar"
+	err := src.Tag(ctx, root, ref)
+	if err != nil {
+		t.Fatal("fail to tag root node", err)
+	}
+
+	// test copy
+	gotDesc, err := oras.Copy(ctx, src, ref, dst, "")
+	if err != nil {
+		t.Fatalf("Copy() error = %v, wantErr %v", err, false)
+	}
+	if !reflect.DeepEqual(gotDesc, root) {
+		t.Errorf("Copy() = %v, want %v", gotDesc, root)
+	}
+
+	// verify contents
+	for i, desc := range descs {
+		exists, err := dst.Exists(ctx, desc)
+		if err != nil {
+			t.Fatalf("dst.Exists(%d) error = %v", i, err)
+		}
+		if !exists {
+			t.Errorf("dst.Exists(%d) = %v, want %v", i, exists, true)
+		}
+	}
+
+	// verify tag
+	gotDesc, err = dst.Resolve(ctx, ref)
+	if err != nil {
+		t.Fatal("dst.Resolve() error =", err)
+	}
+	if !reflect.DeepEqual(gotDesc, root) {
+		t.Errorf("dst.Resolve() = %v, want %v", gotDesc, root)
+	}
 }
 
 func TestCopyGraph_FullCopy(t *testing.T) {
