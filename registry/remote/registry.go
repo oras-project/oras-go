80a8df265653c7e7a235b630c8a3a474a1d8f7b2
Shiwei Zhang <shizh@microsoft.com>
2021-12-16 19:49:01 +0800

    correct package doc
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- registry/remote/registry.go
+++ registry/remote/registry.go
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-// Package distribution provides a client to the remote registry.
+// Package remote provides a client to the remote registry.
 // Reference: https://github.com/distribution/distribution
 package remote
 
