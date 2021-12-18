3725c4fca90515a253e4c4c658219560f25b6472
Shiwei Zhang <shizh@microsoft.com>
2021-12-18 11:37:30 +0800

    improve and doc cache
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- registry/remote/auth/cache.go
+++ registry/remote/auth/cache.go
@@ -3,71 +3,85 @@ package auth
 import (
 	"context"
 	"fmt"
+	"strings"
 	"sync"
 
 	"oras.land/oras-go/v2/errdef"
 	"oras.land/oras-go/v2/internal/syncutil"
 )
 
+// DefaultCache is the sharable cache used by DefaultClient.
 var DefaultCache Cache = NewCache()
 
+// Cache caches the auth-scheme and auth-token for the "Authorization" header in
+// accessing the remote registry.
+// Precisely, the header is `Authorization: auth-scheme auth-token`.
+// The `auth-token` is a generic term as `token68` in RFC 7235 section 2.1.
 type Cache interface {
+	// GetScheme returns the auth-scheme part cached for the given registry.
+	// A single registry is assumed to have a consistent scheme.
+	// If a registry has different schemes per path, the auth client is still
+	// workable. However, the cache may not be effective as the cache cannot
+	// correctly guess the scheme.
 	GetScheme(ctx context.Context, registry string) (Scheme, error)
+
+	// GetToken returns the auth-token part cached for the given registry of a
+	// given scheme.
+	// The underlying implementation MAY cache the token for all schemes for the
+	// given registry.
 	GetToken(ctx context.Context, registry string, scheme Scheme, key string) (string, error)
+
+	// Set fetches the token using the given fetch function and caches the token
+	// for the given scheme with the given key for the given registry.
+	// The return values of the fetch function is returned by this function.
+	// The underlying implementation MAY combine the fetch operation if the Set
+	// function is invoked multiple times at the same time.
 	Set(ctx context.Context, registry string, scheme Scheme, key string, fetch func(context.Context) (string, error)) (string, error)
 }
 
-type basicCache string
+// cacheEntry is a cache entry for a single registry.
+type cacheEntry struct {
+	scheme Scheme
+	tokens sync.Map // map[string]string
+}
 
-type tokenCache sync.Map // map[string]string
-
+// concurrentCache is a cache suitable for concurrent invocation.
 type concurrentCache struct {
-	status    sync.Map // map[string]fetchOnceFunc
-	cacheLock sync.RWMutex
-	cache     map[string]interface{}
+	status sync.Map // map[string]*syncutil.Once
+	cache  sync.Map // map[string]*cacheEntry
 }
 
+// NewCache creates a new go-routine safe cache instance.
 func NewCache() Cache {
-	return &concurrentCache{
-		cache: make(map[string]interface{}),
-	}
+	return &concurrentCache{}
 }
 
+// GetScheme returns the auth-scheme part cached for the given registry.
 func (cc *concurrentCache) GetScheme(ctx context.Context, registry string) (Scheme, error) {
-	cc.cacheLock.RLock()
-	value, ok := cc.cache[registry]
-	cc.cacheLock.RUnlock()
+	entry, ok := cc.cache.Load(registry)
 	if !ok {
 		return SchemeUnknown, errdef.ErrNotFound
 	}
-	switch value.(type) {
-	case *basicCache:
-		return SchemeBasic, nil
-	case *tokenCache:
-		return SchemeBearer, nil
-	}
-	return SchemeUnknown, errdef.ErrNotFound
+	return entry.(*cacheEntry).scheme, nil
 }
 
+// GetToken returns the auth-token part cached for the given registry of a given
+// scheme.
 func (cc *concurrentCache) GetToken(ctx context.Context, registry string, scheme Scheme, key string) (string, error) {
-	cc.cacheLock.RLock()
-	value, ok := cc.cache[registry]
-	cc.cacheLock.RUnlock()
+	entry, ok := cc.cache.Load(registry)
 	if !ok {
 		return "", errdef.ErrNotFound
 	}
-	switch c := value.(type) {
-	case *basicCache:
-		return *(*string)(c), nil
-	case *tokenCache:
-		token, ok := (*sync.Map)(c).Load(key)
-		if ok {
-			return token.(string), nil
-		}
+	if token, ok := entry.(*cacheEntry).tokens.Load(key); ok {
+		return token.(string), nil
 	}
 	return "", errdef.ErrNotFound
 }
 
+// Set fetches the token using the given fetch function and caches the token
+// for the given scheme with the given key for the given registry.
+// Set combines the fetch operation if the Set is invoked multiple times at the
+// same time.
 func (cc *concurrentCache) Set(ctx context.Context, registry string, scheme Scheme, key string, fetch func(context.Context) (string, error)) (string, error) {
 	switch scheme {
 	case SchemeBasic, SchemeBearer:
@@ -75,10 +89,15 @@ func (cc *concurrentCache) Set(ctx context.Context, registry string, scheme Sche
 		return "", fmt.Errorf("unknown scheme: %s", scheme)
 	}
 
-	statusKey := scheme.String() + " " + key
+	// fetch token
+	statusKey := strings.Join([]string{
+		registry,
+		scheme.String(),
+		key,
+	}, " ")
 	statusValue, _ := cc.status.LoadOrStore(statusKey, syncutil.NewOnce())
-	aggregatedFetch := statusValue.(*syncutil.Once)
-	fetchedFirst, result, err := aggregatedFetch.Do(ctx, func() (interface{}, error) {
+	fetchOnce := statusValue.(*syncutil.Once)
+	fetchedFirst, result, err := fetchOnce.Do(ctx, func() (interface{}, error) {
 		return fetch(ctx)
 	})
 	if fetchedFirst {
@@ -92,35 +111,27 @@ func (cc *concurrentCache) Set(ctx context.Context, registry string, scheme Sche
 		return token, nil
 	}
 
-	switch scheme {
-	case SchemeBasic:
-		cc.cacheLock.Lock()
-		cc.cache[registry] = token
-		cc.cacheLock.Unlock()
-	case SchemeBearer:
-		cc.cacheLock.Lock()
-		scopes, ok := cc.cache[registry].(*tokenCache)
-		if !ok {
-			scopes = &tokenCache{}
-			cc.cache[registry] = scopes
-		}
-		cc.cacheLock.Unlock()
-		(*sync.Map)(scopes).Store(key, token)
-	}
+	// cache token
+	entry, _ := cc.cache.LoadOrStore(registry, &cacheEntry{})
+	entry.(*cacheEntry).tokens.Store(key, token)
 
 	return token, nil
 }
 
+// noCache is a cache implementation that does not do cache at all.
 type noCache struct{}
 
+// GetScheme always returns not found error as it has no cache.
 func (noCache) GetScheme(ctx context.Context, registry string) (Scheme, error) {
 	return SchemeUnknown, errdef.ErrNotFound
 }
 
+// GetToken always returns not found error as it has no cache.
 func (noCache) GetToken(ctx context.Context, registry string, scheme Scheme, key string) (string, error) {
 	return "", errdef.ErrNotFound
 }
 
+// Set calls fetch directly without caching.
 func (noCache) Set(ctx context.Context, registry string, scheme Scheme, key string, fetch func(context.Context) (string, error)) (string, error) {
 	return fetch(ctx)
 }
