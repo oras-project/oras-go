1aca37d4442ad4f4055917bf58fe52d4eab0a5d7
Shiwei Zhang <shizh@microsoft.com>
2021-12-19 20:15:57 +0800

    more test cases

--- registry/remote/auth/client.go
+++ registry/remote/auth/client.go
@@ -114,6 +114,9 @@ func (c *Client) SetUserAgent(userAgent string) {
 
 // Do sends the request to the remote server with resolving authentication
 // attempted.
+// On authentication failure due to bad credential,
+// - Do returns error if it fails to fetch token for bearer auth.
+// - Do returns the registry response without error for basic auth.
 func (c *Client) Do(originalReq *http.Request) (*http.Response, error) {
 	ctx := originalReq.Context()
 	req := originalReq.Clone(ctx)
