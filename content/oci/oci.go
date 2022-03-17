8219521396885b65b43b0b26c7aefced5eaa032d
Sajay Antony <1821104+sajayantony@users.noreply.github.com>
2022-03-17 02:03:11 -0700

    Fix godoc output and add package overviews (#116)
    Signed-off-by: Sajay Antony <sajaya@microsoft.com>
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- content/oci/oci.go
+++ content/oci/oci.go
@@ -12,6 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// Package oci provides access to an OCI content store.
+// Reference: https://github.com/opencontainers/image-spec/blob/main/image-layout.md
 package oci
 
 import (
--- registry/remote/registry.go
+++ registry/remote/registry.go
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 // Package remote provides a client to the remote registry.
 // Reference: https://github.com/distribution/distribution
 package remote
