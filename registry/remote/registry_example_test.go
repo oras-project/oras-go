75cb50a0acf9cdfd123cf71c36adabcb44a351b3
Billy Zha <jinzha1@microsoft.com>
2022-03-23 23:53:09 +0800

    Correct package name
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- registry/remote/registry_example_test.go
+++ registry/remote/registry_example_test.go
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package registry_test gives examples code of functions in the registry package.
+// Package registry_test gives examples code of functions in the remote package.
 package remote_test
 
 import (
