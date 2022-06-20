d8e50271923092d6d8cf5ba86546ed6e2bc2fa97
Patrick Zheng <patrickzheng@microsoft.com>
2022-06-20 14:17:02 +0800

    fixed issue 177
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- registry/remote/repository.go
+++ registry/remote/repository.go
@@ -506,6 +506,7 @@ func (s *blobStore) Push(ctx context.Context, expected ocispec.Descriptor, conte
 	if err != nil {
 		return err
 	}
+	hs := strings.Split(req.URL.Host, ":")
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -522,6 +523,10 @@ func (s *blobStore) Push(ctx context.Context, expected ocispec.Descriptor, conte
 	location, err := resp.Location()
 	if err != nil {
 		return err
+	}
+	// work-around solution for https://github.com/oras-project/oras-go/issues/177
+	if len(hs) == 2 && hs[1] == "443" {
+		location.Host = strings.Join(hs, ":")
 	}
 	url = location.String()
 	req, err = http.NewRequestWithContext(ctx, http.MethodPut, url, content)
