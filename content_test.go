5072ebbb14ae889a420530417a3e2aa10fa55a70
Patrick Zheng <patrickzheng@microsoft.com>
2022-07-07 12:07:27 +0800

    added more unit tests for oras.Tag
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- content_test.go
+++ content_test.go
@@ -19,16 +19,21 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/opencontainers/go-digest"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content/memory"
+	"oras.land/oras-go/v2/registry/remote"
 )
 
-func TestRegistry_Tag(t *testing.T) {
+func TestTag_Memory(t *testing.T) {
 	target := memory.New()
 	// generate test content
 	var blobs [][]byte
@@ -86,5 +91,98 @@ func TestRegistry_Tag(t *testing.T) {
 	}
 	if !reflect.DeepEqual(gotDesc, manifestDesc) {
 		t.Errorf("target.Resolve() = %v, want %v", gotDesc, manifestDesc)
+	}
+}
+
+func TestTag_Repository(t *testing.T) {
+	index := []byte(`{"manifests":[]}`)
+	indexDesc := ocispec.Descriptor{
+		MediaType: ocispec.MediaTypeImageIndex,
+		Digest:    digest.FromBytes(index),
+		Size:      int64(len(index)),
+	}
+	src := "foobar"
+	dst := "myTag"
+	var gotIndex []byte
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.Method == http.MethodGet && (r.URL.Path == "/v2/test/manifests/"+indexDesc.Digest.String() || r.URL.Path == "/v2/test/manifests/"+src):
+			if accept := r.Header.Get("Accept"); !strings.Contains(accept, indexDesc.MediaType) {
+				t.Errorf("manifest not convertable: %s", accept)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			w.Header().Set("Content-Type", indexDesc.MediaType)
+			w.Header().Set("Docker-Content-Digest", indexDesc.Digest.String())
+			if _, err := w.Write(index); err != nil {
+				t.Errorf("failed to write %q: %v", r.URL, err)
+			}
+		case r.Method == http.MethodPut && r.URL.Path == "/v2/test/manifests/"+dst:
+			if contentType := r.Header.Get("Content-Type"); contentType != indexDesc.MediaType {
+				w.WriteHeader(http.StatusBadRequest)
+				break
+			}
+			buf := bytes.NewBuffer(nil)
+			if _, err := buf.ReadFrom(r.Body); err != nil {
+				t.Errorf("fail to read: %v", err)
+			}
+			gotIndex = buf.Bytes()
+			w.Header().Set("Docker-Content-Digest", indexDesc.Digest.String())
+			w.WriteHeader(http.StatusCreated)
+		default:
+			t.Errorf("unexpected access: %s %s", r.Method, r.URL)
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
+	defer ts.Close()
+	uri, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatalf("invalid test http server: %v", err)
+	}
+
+	repoName := uri.Host + "/test"
+	repo, err := remote.NewRepository(repoName)
+	if err != nil {
+		t.Fatalf("NewRepository() error = %v", err)
+	}
+	repo.PlainHTTP = true
+	ctx := context.Background()
+
+	// test with manifest tag
+	err = oras.Tag(ctx, repo, src, dst)
+	if err != nil {
+		t.Fatalf("Repository.TagReference() error = %v", err)
+	}
+	if !bytes.Equal(gotIndex, index) {
+		t.Errorf("Repository.TagReference() = %v, want %v", gotIndex, index)
+	}
+
+	// test with manifest digest
+	err = oras.Tag(ctx, repo, indexDesc.Digest.String(), dst)
+	if err != nil {
+		t.Fatalf("Repository.TagReference() error = %v", err)
+	}
+	if !bytes.Equal(gotIndex, index) {
+		t.Errorf("Repository.TagReference() = %v, want %v", gotIndex, index)
+	}
+
+	// test with manifest tag@digest
+	tagDigestRef := src + "@" + indexDesc.Digest.String()
+	err = oras.Tag(ctx, repo, tagDigestRef, dst)
+	if err != nil {
+		t.Fatalf("Repository.TagReference() error = %v", err)
+	}
+	if !bytes.Equal(gotIndex, index) {
+		t.Errorf("Repository.TagReference() = %v, want %v", gotIndex, index)
+	}
+
+	// test with manifest FQDN
+	fqdnRef := repoName + ":" + tagDigestRef
+	err = oras.Tag(ctx, repo, fqdnRef, dst)
+	if err != nil {
+		t.Fatalf("Repository.TagReference() error = %v", err)
+	}
+	if !bytes.Equal(gotIndex, index) {
+		t.Errorf("Repository.TagReference() = %v, want %v", gotIndex, index)
 	}
 }
