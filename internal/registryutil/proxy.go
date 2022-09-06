37c8d817d3a6c3b17a166c41f83608ba3797c8a3
Sylvia Lei <lixlei@microsoft.com>
2022-09-06 18:56:50 +0800

    fix
    Signed-off-by: Sylvia Lei <lixlei@microsoft.com>

--- internal/registryutil/proxy.go
+++ internal/registryutil/proxy.go
@@ -76,6 +76,9 @@ func (p *Proxy) FetchReference(ctx context.Context, reference string) (ocispec.D
 	go func() {
 		defer wg.Done()
 		pushErr = p.Cache.Push(ctx, target, pr)
+		if pushErr != nil {
+			pr.CloseWithError(pushErr)
+		}
 	}()
 	closer := ioutil.CloserFunc(func() error {
 		rcErr := rc.Close()
