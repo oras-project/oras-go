3f4848d7b13493380f565a3a8869a513867cb97a
Xiaoxuan Wang <wangxiaoxuan119@gmail.com>
2023-10-12 13:29:44 +0000

    refined the second test
    Signed-off-by: Xiaoxuan Wang <wangxiaoxuan119@gmail.com>

--- internal/graph/memory_test.go
+++ internal/graph/memory_test.go
@@ -43,7 +43,7 @@ import (
 //	v                       v v
 //
 // C(layer)                  D(layer)
-func TestDeletableMemory_IndexAndRemove(t *testing.T) {
+func TestMemory_IndexAndRemove(t *testing.T) {
 	testFetcher := cas.NewMemory()
 	testMemory := NewMemory()
 	ctx := context.Background()
@@ -313,9 +313,9 @@ B(manifest)   C(manifest) D (manifest)
 |             |           v
 +-->E(layer)<-+------->F(layer)
 */
-func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
+func TestMemory_IndexAllAndPredecessors(t *testing.T) {
 	testFetcher := cas.NewMemory()
-	testDeletableMemory := NewMemory()
+	testMemory := NewMemory()
 	ctx := context.Background()
 
 	// generate test content
@@ -356,12 +356,12 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 	descC := generateManifest(descriptors[0:2]...)                   // blobs[3], manifest "C"
 	descD := generateManifest(descriptors[1:2]...)                   // blobs[4], manifest "D"
 	descA := generateIndex(descriptors[2:5]...)                      // blobs[5], index "A"
-	testFetcher.Push(ctx, descA, bytes.NewReader(blobs[5]))
-	testFetcher.Push(ctx, descB, bytes.NewReader(blobs[2]))
-	testFetcher.Push(ctx, descC, bytes.NewReader(blobs[3]))
-	testFetcher.Push(ctx, descD, bytes.NewReader(blobs[4]))
-	testFetcher.Push(ctx, descE, bytes.NewReader(blobs[0]))
-	testFetcher.Push(ctx, descF, bytes.NewReader(blobs[1]))
+
+	// prepare the content in the fetcher, so that it can be used to test IndexAll
+	testContents := []ocispec.Descriptor{descE, descF, descB, descC, descD, descA}
+	for i := 0; i < len(blobs); i++ {
+		testFetcher.Push(ctx, testContents[i], bytes.NewReader(blobs[i]))
+	}
 
 	// make sure that testFetcher works
 	rc, err := testFetcher.Fetch(ctx, descA)
@@ -380,11 +380,6 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 		t.Errorf("testFetcher.Fetch() = %v, want %v", got, blobs[4])
 	}
 
-	// index node A into graph.DeletableMemory using IndexAll
-	testDeletableMemory.IndexAll(ctx, testFetcher, descA)
-
-	// check that the content of testDeletableMemory.predecessors and successors
-	// are correct
 	nodeKeyA := descriptor.FromOCI(descA)
 	nodeKeyB := descriptor.FromOCI(descB)
 	nodeKeyC := descriptor.FromOCI(descC)
@@ -392,11 +387,22 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 	nodeKeyE := descriptor.FromOCI(descE)
 	nodeKeyF := descriptor.FromOCI(descF)
 
+	// index node A into testMemory using IndexAll
+	testMemory.IndexAll(ctx, testFetcher, descA)
+
 	// check the information of node A
-	predecessorsA := testDeletableMemory.predecessors[nodeKeyA]
-	successorsA := testDeletableMemory.successors[nodeKeyA]
-	for range predecessorsA {
-		t.Errorf("predecessors of %s should be empty", "A")
+	// 1. verify that node A exists in testMemory.nodes
+	if _, exists := testMemory.nodes[nodeKeyA]; !exists {
+		t.Errorf("nodes entry of %s should exist", "A")
+	}
+	// 2. verify that there is no entry of A in predecessors
+	if _, exists := testMemory.predecessors[nodeKeyA]; exists {
+		t.Errorf("there should be no entry of %s in predecessors", "A")
+	}
+	// 3. verify that A has successors B, C, D
+	successorsA, exists := testMemory.successors[nodeKeyA]
+	if !exists {
+		t.Errorf("there should be an entry of %s in successors", "A")
 	}
 	if !successorsA.Contains(nodeKeyB) {
 		t.Errorf("successors of %s should contain %s", "A", "B")
@@ -409,21 +415,33 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 	}
 
 	// check the information of node B
-	predecessorsB := testDeletableMemory.predecessors[nodeKeyB]
-	successorsB := testDeletableMemory.successors[nodeKeyB]
+	// 1. verify that node B exists in testMemory.nodes
+	if _, exists := testMemory.nodes[nodeKeyB]; !exists {
+		t.Errorf("nodes entry of %s should exist", "B")
+	}
+	// 2. verify that B has node A in its predecessors
+	predecessorsB := testMemory.predecessors[nodeKeyB]
 	if !predecessorsB.Contains(nodeKeyA) {
 		t.Errorf("predecessors of %s should contain %s", "B", "A")
 	}
+	// 3. verify that B has node E in its successors
+	successorsB := testMemory.successors[nodeKeyB]
 	if !successorsB.Contains(nodeKeyE) {
 		t.Errorf("successors of %s should contain %s", "B", "E")
 	}
 
 	// check the information of node C
-	predecessorsC := testDeletableMemory.predecessors[nodeKeyC]
-	successorsC := testDeletableMemory.successors[nodeKeyC]
+	// 1. verify that node C exists in testMemory.nodes
+	if _, exists := testMemory.nodes[nodeKeyC]; !exists {
+		t.Errorf("nodes entry of %s should exist", "C")
+	}
+	// 2. verify that C has node A in its predecessors
+	predecessorsC := testMemory.predecessors[nodeKeyC]
 	if !predecessorsC.Contains(nodeKeyA) {
 		t.Errorf("predecessors of %s should contain %s", "C", "A")
 	}
+	// 3. verify that C has node E and F in its successors
+	successorsC := testMemory.successors[nodeKeyC]
 	if !successorsC.Contains(nodeKeyE) {
 		t.Errorf("successors of %s should contain %s", "C", "E")
 	}
@@ -432,43 +450,73 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 	}
 
 	// check the information of node D
-	predecessorsD := testDeletableMemory.predecessors[nodeKeyD]
-	successorsD := testDeletableMemory.successors[nodeKeyD]
+	// 1. verify that node D exists in testMemory.nodes
+	if _, exists := testMemory.nodes[nodeKeyD]; !exists {
+		t.Errorf("nodes entry of %s should exist", "D")
+	}
+	// 2. verify that D has node A in its predecessors
+	predecessorsD := testMemory.predecessors[nodeKeyD]
 	if !predecessorsD.Contains(nodeKeyA) {
 		t.Errorf("predecessors of %s should contain %s", "D", "A")
 	}
+	// 3. verify that D has node F in its successors
+	successorsD := testMemory.successors[nodeKeyD]
 	if !successorsD.Contains(nodeKeyF) {
 		t.Errorf("successors of %s should contain %s", "D", "F")
 	}
 
 	// check the information of node E
-	predecessorsE := testDeletableMemory.predecessors[nodeKeyE]
-	successorsE := testDeletableMemory.successors[nodeKeyE]
+	// 1. verify that node E exists in testMemory.nodes
+	if _, exists := testMemory.nodes[nodeKeyE]; !exists {
+		t.Errorf("nodes entry of %s should exist", "E")
+	}
+	// 2. verify that E has node B and C in its predecessors
+	predecessorsE := testMemory.predecessors[nodeKeyE]
 	if !predecessorsE.Contains(nodeKeyB) {
 		t.Errorf("predecessors of %s should contain %s", "E", "B")
 	}
 	if !predecessorsE.Contains(nodeKeyC) {
 		t.Errorf("predecessors of %s should contain %s", "E", "C")
 	}
-	for range successorsE {
+	// 3. verify that E has an entry in successors and it's empty
+	successorsE, exists := testMemory.successors[nodeKeyE]
+	if !exists {
+		t.Errorf("entry %s should exist in testMemory.successors", "E")
+	}
+	if successorsE == nil {
+		t.Errorf("successors of %s should be an empty set, not nil", "E")
+	}
+	if len(successorsE) != 0 {
 		t.Errorf("successors of %s should be empty", "E")
 	}
 
 	// check the information of node F
-	predecessorsF := testDeletableMemory.predecessors[nodeKeyF]
-	successorsF := testDeletableMemory.successors[nodeKeyF]
+	// 1. verify that node F exists in testMemory.nodes
+	if _, exists := testMemory.nodes[nodeKeyF]; !exists {
+		t.Errorf("nodes entry of %s should exist", "F")
+	}
+	// 2. verify that F has node C and D in its predecessors
+	predecessorsF := testMemory.predecessors[nodeKeyF]
 	if !predecessorsF.Contains(nodeKeyC) {
 		t.Errorf("predecessors of %s should contain %s", "F", "C")
 	}
 	if !predecessorsF.Contains(nodeKeyD) {
 		t.Errorf("predecessors of %s should contain %s", "F", "D")
 	}
-	for range successorsF {
-		t.Errorf("successors of %s should be empty", "E")
+	// 3. verify that F has an entry in successors and it's empty
+	successorsF, exists := testMemory.successors[nodeKeyF]
+	if !exists {
+		t.Errorf("entry %s should exist in testMemory.successors", "F")
+	}
+	if successorsF == nil {
+		t.Errorf("successors of %s should be an empty set, not nil", "F")
+	}
+	if len(successorsF) != 0 {
+		t.Errorf("successors of %s should be empty", "F")
 	}
 
 	// check the Predecessors of node C is node A
-	predsC, err := testDeletableMemory.Predecessors(ctx, descC)
+	predsC, err := testMemory.Predecessors(ctx, descC)
 	if err != nil {
 		t.Errorf("testFetcher.Predecessors() error = %v", err)
 	}
@@ -481,7 +529,7 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 	}
 
 	// check the Predecessors of node F are node C and node D
-	predsF, err := testDeletableMemory.Predecessors(ctx, descF)
+	predsF, err := testMemory.Predecessors(ctx, descF)
 	if err != nil {
 		t.Errorf("testFetcher.Predecessors() error = %v", err)
 	}
@@ -496,7 +544,7 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 	}
 
 	// remove node C and check the stored information
-	testDeletableMemory.Remove(ctx, descC)
+	testMemory.Remove(ctx, descC)
 	if predecessorsE.Contains(nodeKeyC) {
 		t.Errorf("predecessors of %s should not contain %s", "E", "C")
 	}
@@ -506,19 +554,22 @@ func TestDeletableMemory_IndexAllAndPredecessors(t *testing.T) {
 	if !successorsA.Contains(nodeKeyC) {
 		t.Errorf("successors of %s should still contain %s", "A", "C")
 	}
-	if _, exists := testDeletableMemory.successors[nodeKeyC]; exists {
+	if _, exists := testMemory.successors[nodeKeyC]; exists {
 		t.Errorf("testDeletableMemory.successors should not contain the entry of %v", "C")
 	}
 
 	// remove node A and check the stored information
-	testDeletableMemory.Remove(ctx, descA)
+	testMemory.Remove(ctx, descA)
 	if predecessorsD.Contains(nodeKeyA) {
 		t.Errorf("predecessors of %s should not contain %s", "D", "A")
 	}
 	if predecessorsB.Contains(nodeKeyA) {
 		t.Errorf("predecessors of %s should not contain %s", "B", "A")
 	}
-	if _, exists := testDeletableMemory.successors[nodeKeyA]; exists {
+	if _, exists := testMemory.predecessors[nodeKeyC]; exists {
+		t.Errorf("entry %s in predecessors should no longer exists", "C")
+	}
+	if _, exists := testMemory.successors[nodeKeyA]; exists {
 		t.Errorf("testDeletableMemory.successors should not contain the entry of %v", "A")
 	}
 }
