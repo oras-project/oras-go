0f1dc30a0fd713f3bafcf3bd52693b2e8a8a6fa5
Lixia (Sylvia) Lei <lixlei@microsoft.com>
2023-10-18 16:04:45 +0800

    perf: reduce auth request count for manifest delete (#618)
    Add `pull` and `delete` scope hints before attempting client-side indexing on manifest delete.
    
    Resolve: #594 
    Signed-off-by: Lixia (Sylvia) Lei <lixlei@microsoft.com>

--- registry/remote/repository.go
+++ registry/remote/repository.go
@@ -1120,6 +1120,7 @@ func (s *manifestStore) deleteWithIndexing(ctx context.Context, target ocispec.D
 		if err := limitSize(target, s.repo.MaxMetadataBytes); err != nil {
 			return err
 		}
+		ctx = auth.AppendRepositoryScope(ctx, s.repo.Reference, auth.ActionPull, auth.ActionDelete)
 		manifestJSON, err := content.FetchAll(ctx, s, target)
 		if err != nil {
 			return err
