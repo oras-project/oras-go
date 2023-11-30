38fd16c1abea0f0a6f2bf135fc941f398165a7b6
Xiaoxuan Wang <wangxiaoxuan119@gmail.com>
2023-11-30 12:54:51 +0800

    added doc comments
    Signed-off-by: Xiaoxuan Wang <wangxiaoxuan119@gmail.com>

--- internal/graph/memory.go
+++ internal/graph/memory.go
@@ -175,6 +175,8 @@ func (m *Memory) index(ctx context.Context, fetcher content.Fetcher, node ocispe
 	return successors, nil
 }
 
+// IsDanglingNode decides whether a node is dangling. A node is considered
+// dangling if it is present and has no predecessors.
 func (m *Memory) IsDanglingNode(desc ocispec.Descriptor) bool {
 	key := descriptor.FromOCI(desc)
 	_, existsInMemory := m.nodes[key]
