fcda80d29ffd07e392fb718ed86b0fa8cca2c310
Shiwei Zhang <shizh@microsoft.com>
2024-11-28 15:22:16 +0800

    feat: add utility functions
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- progress/manager.go
+++ progress/manager.go
@@ -14,6 +14,23 @@ type Manager interface {
 	Track(desc ocispec.Descriptor) (Tracker, error)
 }
 
+// ManagerFunc is an adapter to allow the use of ordinary functions as Managers.
+// If f is a function with the appropriate signature, ManagerFunc(f) is a
+// [Manager] that calls f.
+type ManagerFunc func(ocispec.Descriptor, Status, error) error
+
+// Track starts tracking the progress of a descriptor.
+func (f ManagerFunc) Track(desc ocispec.Descriptor) (Tracker, error) {
+	return TrackerFunc(func(status Status, err error) error {
+		return f(desc, status, err)
+	}), nil
+}
+
+// Close closes the manager.
+func (f ManagerFunc) Close() error {
+	return nil
+}
+
 // Record adds the progress of a descriptor as a single entry.
 func Record(m Manager, desc ocispec.Descriptor, status Status) error {
 	tracker, err := m.Track(desc)
