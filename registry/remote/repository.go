60235a9e3dac2af9f0240b4ce2799aa8544c4d86
Lixia (Sylvia) Lei <lixlei@microsoft.com>
2025-01-20 19:05:03 +0800

    set cov threshold to 80
    Signed-off-by: Lixia (Sylvia) Lei <lixlei@microsoft.com>

--- registry/remote/repository.go
+++ registry/remote/repository.go
@@ -1608,6 +1608,7 @@ func (s *manifestStore) generateDescriptor(resp *http.Response, ref registry.Ref
 // calculateDigestFromResponse calculates the actual digest of the response body
 // taking care not to destroy it in the process.
 func calculateDigestFromResponse(resp *http.Response, maxMetadataBytes int64) (digest.Digest, error) {
+	// TODO: move this to another PR
 	if resp.Body == nil {
 		return "", fmt.Errorf("%s %q: response body is nil", resp.Request.Method, resp.Request.URL)
 	}
