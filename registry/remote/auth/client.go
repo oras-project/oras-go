acb359473673f60c3d44eea75d277d16d842aea5
Terry Howe <terrylhowe@gmail.com>
2026-09-16 19:03:11 -0600

    Update registry/remote/auth/client.go
    Signed-off-by: Terry Howe <terrylhowe@gmail.com>

--- registry/remote/auth/client.go
+++ registry/remote/auth/client.go
@@ -275,7 +275,7 @@ func (c *Client) Do(originalReq *http.Request) (*http.Response, error) {
 	// attempt cached auth token
 	var attemptedKey string
 	cache := c.cache()
-	host := requestResource(originalReq).Registry
+	host := requestResource(originalReq).Host()
 	scheme, err := cache.GetScheme(ctx, host)
 	if err == nil {
 		switch scheme {
